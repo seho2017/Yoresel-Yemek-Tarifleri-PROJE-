223d6608cccbd80854ba3fb74e1eec36aeee3ad6
seho2017 <149900043+seho2017@users.noreply.github.com>
2023-12-07 23:15:12 +0300

    tarif yolla botunu
    taslak ve kodlar yazıldı sadece kaydet botunu aktifleştırmedim

---
+++ Form6.Designer.cs
@@ -0,0 +1,272 @@
+﻿namespace MASA_ÜSTÜ_YÖRESEL_YEMEKLER
+{
+    partial class Form6
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form6));
+            button1 = new Button();
+            pictureBox1 = new PictureBox();
+            button2 = new Button();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            textBox6 = new TextBox();
+            button3 = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.BackgroundImage = (Image)resources.GetObject("button1.BackgroundImage");
+            button1.BackgroundImageLayout = ImageLayout.Zoom;
+            button1.FlatAppearance.BorderSize = 0;
+            button1.Location = new Point(-1, -3);
+            button1.Name = "button1";
+            button1.Size = new Size(55, 50);
+            button1.TabIndex = 0;
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // pictureBox1
+            // 
+            pictureBox1.BackColor = SystemColors.ButtonShadow;
+            pictureBox1.Location = new Point(-1, -3);
+            pictureBox1.Name = "pictureBox1";
+            pictureBox1.Size = new Size(646, 50);
+            pictureBox1.TabIndex = 1;
+            pictureBox1.TabStop = false;
+            // 
+            // button2
+            // 
+            button2.BackgroundImage = (Image)resources.GetObject("button2.BackgroundImage");
+            button2.BackgroundImageLayout = ImageLayout.Zoom;
+            button2.Location = new Point(590, -3);
+            button2.Name = "button2";
+            button2.Size = new Size(55, 50);
+            button2.TabIndex = 2;
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label1.ForeColor = Color.FromArgb(255, 128, 0);
+            label1.Location = new Point(0, 51);
+            label1.Name = "label1";
+            label1.Size = new Size(96, 30);
+            label1.TabIndex = 3;
+            label1.Text = "Şehir Adı";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.ForeColor = Color.FromArgb(255, 128, 0);
+            label2.Location = new Point(0, 128);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 30);
+            label2.TabIndex = 4;
+            label2.Text = "Tarif Adı";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.ForeColor = Color.FromArgb(255, 128, 0);
+            label3.Location = new Point(-1, 205);
+            label3.Name = "label3";
+            label3.Size = new Size(167, 30);
+            label3.TabIndex = 5;
+            label3.Text = "Hazırlama Süresi";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.ForeColor = Color.FromArgb(255, 128, 0);
+            label4.Location = new Point(0, 296);
+            label4.Name = "label4";
+            label4.Size = new Size(141, 30);
+            label4.TabIndex = 6;
+            label4.Text = "Pişirme Süresi";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.ForeColor = Color.FromArgb(255, 128, 0);
+            label5.Location = new Point(-1, 369);
+            label5.Name = "label5";
+            label5.Size = new Size(121, 30);
+            label5.TabIndex = 7;
+            label5.Text = "Malzemeler";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.BackColor = Color.FromArgb(224, 224, 224);
+            label6.Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.ForeColor = Color.FromArgb(255, 128, 0);
+            label6.Location = new Point(0, 449);
+            label6.Name = "label6";
+            label6.Size = new Size(108, 30);
+            label6.TabIndex = 8;
+            label6.Text = "Hazırlanışı";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(-1, 84);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(646, 50);
+            textBox1.TabIndex = 9;
+            textBox1.MouseEnter += textBox1_MouseEnter;
+            textBox1.MouseLeave += textBox1_MouseLeave;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(-1, 161);
+            textBox2.Multiline = true;
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(646, 54);
+            textBox2.TabIndex = 10;
+            textBox2.MouseEnter += textBox2_MouseEnter;
+            textBox2.MouseLeave += textBox2_MouseLeave;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(-1, 238);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(646, 55);
+            textBox3.TabIndex = 11;
+            textBox3.MouseEnter += textBox3_MouseEnter;
+            textBox3.MouseLeave += textBox3_MouseLeave;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(-1, 329);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(646, 49);
+            textBox4.TabIndex = 12;
+            textBox4.MouseEnter += textBox4_MouseEnter;
+            textBox4.MouseLeave += textBox4_MouseLeave;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(-1, 402);
+            textBox5.Multiline = true;
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(646, 53);
+            textBox5.TabIndex = 13;
+            textBox5.MouseEnter += textBox5_MouseEnter;
+            textBox5.MouseLeave += textBox5_MouseLeave;
+            // 
+            // textBox6
+            // 
+            textBox6.Location = new Point(-1, 482);
+            textBox6.Multiline = true;
+            textBox6.Name = "textBox6";
+            textBox6.Size = new Size(646, 56);
+            textBox6.TabIndex = 14;
+            textBox6.MouseEnter += textBox6_MouseEnter;
+            textBox6.MouseLeave += textBox6_MouseLeave;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(255, 128, 0);
+            button3.Location = new Point(-1, 539);
+            button3.Name = "button3";
+            button3.Size = new Size(646, 45);
+            button3.TabIndex = 15;
+            button3.Text = "TASLAĞI KAYDET";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // Form6
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = SystemColors.ButtonFace;
+            ClientSize = new Size(644, 583);
+            Controls.Add(button3);
+            Controls.Add(textBox6);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(pictureBox1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Form6";
+            Text = "Form6";
+            MouseDown += Form6_MouseDown;
+            MouseMove += Form6_MouseMove;
+            MouseUp += Form6_MouseUp;
+            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private PictureBox pictureBox1;
+        private Button button2;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private TextBox textBox6;
+        private Button button3;
+    }
+}
---
+++ Form6.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MASA_ÜSTÜ_YÖRESEL_YEMEKLER
+{
+    public partial class Form6 : Form
+    {
+        public Form6()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            Form2 Tarif = new Form2();
+            Tarif.Show();
+            this.Hide();
+        }
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+        bool move;
+        int mouse_x;
+        int mouse_y;
+        private void Form6_MouseDown(object sender, MouseEventArgs e)
+        {
+            //mouse hareketi icin;
+            move = true;
+            mouse_x = e.X;
+            mouse_y = e.Y;
+        }
+
+        private void Form6_MouseMove(object sender, MouseEventArgs e)
+        {
+
+            // localasyonla ilgilenecek maouse hareketiyle hareket
+            if (move)
+            {
+                this.SetDesktopLocation(MousePosition.X - mouse_x, MousePosition.Y - mouse_y);
+            }
+        }
+
+        private void Form6_MouseUp(object sender, MouseEventArgs e)
+        {
+            //mouse hareket
+            move = false;
+        }
+
+        private void textBox1_MouseEnter(object sender, EventArgs e)
+        {
+            // Fare TextBox'ın üzerine gelince rengi değiştir
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = Color.LightBlue;
+        }
+
+        private void textBox1_MouseLeave(object sender, EventArgs e)
+        {
+            // Fare TextBox'ın üzerinden çıkınca orijinal rengini geri al
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = SystemColors.Window;
+        }
+
+        private void textBox2_MouseEnter(object sender, EventArgs e)
+        {
+            // Fare TextBox'ın üzerine gelince rengi değiştir
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = Color.LightBlue;
+        }
+
+        private void textBox2_MouseLeave(object sender, EventArgs e)
+        {
+            // Fare TextBox'ın üzerinden çıkınca orijinal rengini geri al
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = SystemColors.Window;
+        }
+
+        private void textBox3_MouseEnter(object sender, EventArgs e)
+        {
+            // Fare TextBox'ın üzerine gelince rengi değiştir
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = Color.LightBlue;
+        }
+
+        private void textBox3_MouseLeave(object sender, EventArgs e)
+        {
+            // Fare TextBox'ın üzerinden çıkınca orijinal rengini geri al
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = SystemColors.Window;
+        }
+
+        private void textBox4_MouseEnter(object sender, EventArgs e)
+        {
+            // Fare TextBox'ın üzerine gelince rengi değiştir
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = Color.LightBlue;
+        }
+
+        private void textBox4_MouseLeave(object sender, EventArgs e)
+        {
+            // Fare TextBox'ın üzerinden çıkınca orijinal rengini geri al
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = SystemColors.Window;
+        }
+
+        private void textBox5_MouseEnter(object sender, EventArgs e)
+        {
+            // Fare TextBox'ın üzerine gelince rengi değiştir
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = Color.LightBlue;
+        }
+
+        private void textBox5_MouseLeave(object sender, EventArgs e)
+        {
+            // Fare TextBox'ın üzerinden çıkınca orijinal rengini geri al
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = SystemColors.Window;
+        }
+
+        private void textBox6_MouseEnter(object sender, EventArgs e)
+        {
+            // Fare TextBox'ın üzerine gelince rengi değiştir
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = Color.LightBlue;
+        }
+
+        private void textBox6_MouseLeave(object sender, EventArgs e)
+        {
+            // Fare TextBox'ın üzerinden çıkınca orijinal rengini geri al
+            TextBox textBox = (TextBox)sender;
+            textBox.BackColor = SystemColors.Window;
+        }
+    }
+}
